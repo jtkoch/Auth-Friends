2cc2595c5d63bf18515813a376affc2e948b1d92
Jensen Koch <jtkoch12@gmail.com>
2020-03-17 19:10:44 -0500

    finished MVP

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import Login from './components/Login'
+import Friends from './components/Friends'
+import CreateFriend from './components/CreateFriend'
 import './App.css';
+import PrivateRoute from "./authorization/PrivateRoute.js"
+import UpdateFriend from './components/UpdateFriend';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Auth Friends</h1>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/friends">Friends</Link>
+          </li>
+          <li>
+            <Link to="/newfriend">Create a New Friend</Link>
+          </li>
+        </ul>
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact path='/' component={Friends} />{/* Friends is the homepage, if not logged in, we'll get redirect to Login */}
+        <PrivateRoute path='/newfriend' component={CreateFriend} />
+        <PrivateRoute path='/updatefriend' component={UpdateFriend} />
+
+      </div>
+    </Router>
   );
 }
 
